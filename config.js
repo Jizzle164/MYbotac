606c24f36d4650ba2bdebbfa99843a4fa745da10
Glitch (midi-sheer-meteor) <none>
2020-09-09 16:30:17 +0000

    ❣️⛺️ Checkpoint ./config.json:15698863/42 ./config.js:15698863/443 ./commands/loop.js:15698863/2

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
-  "ownerID": "692790523424931932",//Bu kısıma, kendi ID'niz. 
-  "sahip": ["692790523424931932"],
+  "ownerID": "564854610309939235",//Bu kısıma, kendi ID'niz. 
+  "sahip": ["564854610309939235"],
   "admins": [""],//Yetkili eklemek isterseniz, o kişinin ID'si.
   "support": [""],//Destek ekibi eklemek isterseniz, o kişinin ID'si.
-  "token": "",//Botun, tokeni.
+  "token": "NzUzMjY1MTg5NDEwOTYzNTI3.X1jq4A.Jn151RXz4jpgCCpt_JBxxjEuSzo",//Botun, tokeni.
   "dashboard" : {
     "oauthSecret": "4G99fsj3tq5X-mkcyL43tsqeodcKePhG",//Client secret.
-    "callbackURL": `https://v12muzik.glitch.me/callback`,//oAuth2 kısmında bulunan kutucuğa, bu bağlantıyı ekleyiniz.
+    "callbackURL": `https://gelis-muzik-botu.glitch.me/callback`,//oAuth2 kısmında bulunan kutucuğa, bu bağlantıyı ekleyiniz.
     "sessionSecret": "Codare",//Bu kısımı, değiştirmeyiniz.
-    "domain": "https://v12muzik.glitch.me"//Sitenin, adresi.
+    "domain": "https://gelis-muzik-botu.glitch.me"//Sitenin, adresi.
   }
 };
 
